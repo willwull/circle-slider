9f863b71918f02fbb81e80c29b2bae81997b7e63
Willy Liu <arkazain@gmail.com>
2018-03-26 22:43:52 +0200

    🚨 Add tests for _radToDeg

--- test/test.js
+++ test/test.js
@@ -1,3 +1,11 @@
-test("hello world", () => {
-  expect("hello world").toBe("hello world");
+const CircleSlider = require("../src/index");
+
+test("_radToDeg converts correctly", () => {
+  // probably overkill to test such a simple method
+  const rtd = CircleSlider._radToDeg;
+  expect(rtd(0)).toBeCloseTo(0);
+  expect(rtd(Math.PI / 6)).toBeCloseTo(30);
+  expect(rtd(Math.PI / 4)).toBeCloseTo(45);
+  expect(rtd(Math.PI / 3)).toBeCloseTo(60);
+  expect(rtd(Math.PI / 2)).toBeCloseTo(90);
 });
