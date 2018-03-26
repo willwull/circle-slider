cb2d7c375c31c80ac620f19d96ec6b9f11ee74c2
Willy Liu <arkazain@gmail.com>
2018-03-26 22:54:04 +0200

    🚨 Add tests for modulo

--- test/test.js
+++ test/test.js
@@ -9,3 +9,12 @@ test("_radToDeg converts correctly", () => {
   expect(rtd(Math.PI / 3)).toBeCloseTo(60);
   expect(rtd(Math.PI / 2)).toBeCloseTo(90);
 });
+
+test("modulo gives correct values", () => {
+  const mod = CircleSlider.modulo;
+  expect(mod(0, 360)).toBe(0);
+  expect(mod(360, 360)).toBe(0);
+  expect(mod(361, 360)).toBe(1);
+  expect(mod(359, 360)).toBe(359);
+  expect(mod(720, 360)).toBe(0);
+});
